68ade178cf94394b379f20bc33a96db074499b5c
Sidnei Barbieri <sidneibarbieri@gmail.com>
2017-07-05 22:07:53 -0300

    Nth commit

---
+++ Projeto01/r-as-a-calculator.R
@@ -0,0 +1,23 @@
+rm(list=ls())
+a <- 12
+b <- 3
+a+b
+
+x = 7
+y = 2
+
+x^y
+
+max(a,b,x,y)
+min(a,b,x,y)
+
+x%%y
+
+x%/%y
+
+(x %% y)
+y * (x %% y)
+(x %% y) + y * (x %% y)
+
+help.search("trigonometry")
+
